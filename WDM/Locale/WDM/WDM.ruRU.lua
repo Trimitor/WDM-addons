97e18a96bab0ed8c7632c3483652aa6733c23ec5
Trimitor <73941033+Trimitor@users.noreply.github.com>
2021-09-30 11:37:49 +0300

    Add files via upload

---
+++ WDM/Locale/WDM/WDM.ruRU.lua
@@ -0,0 +1,20 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("WDM", "ruRU")
+if not L then return end
+
+L["POI Icons"] = "Иконки POI"
+L["Additional POI (Point of Interest) icons. Standard icons are not affected"] = "Дополнительные иконки POI (Point of Interest). Стандартные иконки не затрагиваются"
+
+L["Show POI icons"] = "Показывать POI"
+L["Toggles the display of POI's on World Map."] = "Переключить отображение дополнительных иконок на карте мира"
+
+L["Show Flight Masters"] = "Показывать Распорядителей полетов"
+L["Toggles the display of Flight Masters on World Map."] = "Переключить отображение Распорядителей полетов на карте мира"
+
+L["Including the opposite faction"] = "В том числе противоположной фракции"
+L["Toggles the display of Flight Masters of the opposite faction"] = "Переключить отображение Распорядителей полетов противоположной фракции"
+
+L["MicroDungeons"] = "Микроподземелья"
+L["Activate only if you have installed an additional patch"] = "Активировать только в том случае, если вы установили дополнительный патч"
+
+L["Show Caves and Mines"] = "Показывать пещеры и шахты"
+L["Warning! Experimental features"] = "Внимание! Экспериментальная функция"
